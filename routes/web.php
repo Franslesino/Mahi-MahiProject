18caec6c5a58223824a156a34a4ffbc99aa7017e
Sabil Aditia <sabiladitia20@gmail.com>
2025-12-16 22:40:18 +0700

    Add instructor.profile route alias

--- routes/web.php
+++ routes/web.php
@@ -472,7 +472,7 @@ Route::middleware('auth')->group(function () {
             Route::get('/dashboard', [InstructorController::class, 'dashboard'])->name('dashboard');
 
             // Profile Management
-            Route::get('/profile', [InstructorProfileController::class, 'edit'])->name('profile.edit');
+            Route::get('/profile', [InstructorProfileController::class, 'edit'])->name('profile');
             Route::put('/profile', [InstructorProfileController::class, 'update'])->name('profile.update');
 
             // Course Management
